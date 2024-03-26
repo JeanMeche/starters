e0af613610590343bdc9981c14c5c49a97770420
Matthieu Riegler <kyro38@gmail.com>
2024-03-25 20:13:41 -0700

    my config

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import 'zone.js';
 
 @Component({
+  selector: 'app-child',
+  standalone: true,
+  template: `Child OK!`,
+})
+export class Child {}
+
+@Component({
   selector: 'app-root',
   standalone: true,
+  imports: [Child],
   template: `
+    <header>Angular {{version}}</header>
     <h1>Hello from {{ name }}!</h1>
     <a target="_blank" href="https://angular.dev/overview">
       Learn more about Angular
     </a>
-  `,
+   `,
 })
 export class App {
+  version = VERSION.full;
   name = 'Angular';
 }
 
